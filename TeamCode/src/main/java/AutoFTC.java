208bdd8b3f0862d9a52ad668e93325e572ef924f
sDiamond123 <sdiamond21@sidwell.edu>
2019-11-17 14:25:34 -0500

    again, I know

---
+++ TeamCode/src/main/java/AutoFTC.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class AutoFTC extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+    }
+}
