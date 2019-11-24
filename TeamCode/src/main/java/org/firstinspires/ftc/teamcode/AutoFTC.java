5f549c077e8ee46eadb30b2d178278368ccb7954
chris omara <57817327+golfclubkiller4@users.noreply.github.com>
2019-11-24 14:53:34 -0500

    jnkj

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFTC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFTC.java
@@ -2,12 +2,69 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous
 public class AutoFTC extends LinearOpMode {
+    DcMotor leftFront, leftRear, rightFront, rightRear;
     @Override
     public void runOpMode() throws InterruptedException {
-
+        //int count =
+        //double power = 1;
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        rightRear = hardwareMap.dcMotor.get("rightRear");
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
+
+        firstbit(1, 1000);
+    }
+    public void firstbit(double power, int distance){
+        //reset encoders
+        ResetEncoders();
+        //set target position
+        SetTargetPosition(distance);
+        //set to Run to position mode
+        SetToRunToPosition();
+        //drive at power
+        Drive(power);
+        while(leftFront.isBusy() && leftRear.isBusy() && rightFront.isBusy() && rightRear.isBusy()){
+
+        }
+        //stops driving
+        StopDriving();
+    }
+    public void StopDriving(){
+        leftFront.setPower(0);
+        leftRear.setPower(0);
+        rightFront.setPower(0);
+        rightRear.setPower(0);
+    }
+    public void Drive(double power){
+        leftFront.setPower(power);
+        leftRear.setPower(power);
+        rightFront.setPower(power);
+        rightRear.setPower(power);
+    }
+    public void ResetEncoders(){
+        leftFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightRear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    public void SetToRunToPosition(){
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightRear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+    public void SetTargetPosition(int distance){
+        leftFront.setTargetPosition(distance);
+        leftRear.setTargetPosition(distance);
+        rightFront.setTargetPosition(distance);
+        rightRear.setTargetPosition(distance);
     }
 }
